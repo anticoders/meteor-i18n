54e6bcc8c80a44735ef0297a72f5a459f531c312
softwarerero <el@softwarerero.com>
2014-02-14 11:21:20 -0300

    if no translation is found i18n returns the code, optionally wrapped by a start and an end token.

--- i18n.js
+++ i18n.js
@@ -12,7 +12,7 @@ i18n = function(label) {
   i18n._dep.depend();
   if(typeof label !== 'string') return '';
   var array = label.split('.');
-  return get(i18n._maps[i18n._language], array) || get(i18n._maps[i18n._default], array) || '';
+  return get(i18n._maps[i18n._language], array) || get(i18n._maps[i18n._default], array) || i18n._missingStartToken + i18n._language + ": " + label + i18n._missingEndToken;
 } 
 
 if(Meteor.isClient) {
@@ -25,6 +25,8 @@ i18n._maps = {};
 
 i18n._default = 'en';
 i18n._language = '';
+i18n._missingStartToken = '';
+i18n._missingEndToken = '';
 i18n._dep = new Deps.Dependency();
 
 i18n.setLanguage = function(language) {
@@ -34,6 +36,10 @@ i18n.setLanguage = function(language) {
 i18n.setDefaultLanguage = function(language) {
   i18n._default = language;
   i18n._dep.changed();
+};
+i18n.setMissingTokens = function(tokens) {
+  i18n._missingStartToken = tokens[0];
+  i18n._missingEndToken = tokens[1];
 };
 i18n.getLanguage = function() {
   i18n._dep.depend();
